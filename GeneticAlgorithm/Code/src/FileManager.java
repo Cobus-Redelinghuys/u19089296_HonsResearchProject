1893d1af1f88773b05a4c86860cfa6ce6c95485a
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-06-11 01:36:56 +0200

    Trying to implement chromosome

--- GeneticAlgorithm/Code/src/FileManager.java
+++ GeneticAlgorithm/Code/src/FileManager.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 import java.util.Random;
+import java.lang.reflect.TypeVariable;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -136,6 +137,68 @@ class GeneticAlgorithmConfig{
     }
 }
 
+class ChromosomeConfig{
+
+}
+
+@SuppressWarnings("rawtypes")
+class GeneConfig{
+    public final GeneDataType geneDataType;
+    private final Object maxValue;
+    private final Object minValue;
+    private final Object[] invalidValues;
+    private final Class dataType;
+    
+    @SuppressWarnings("rawtypes")
+    public GeneConfig(JSONObject jsonObject){
+        Object res = null;
+        try{
+            String line = (String)jsonObject.get("DataType");
+            for(GeneDataType geneDT: GeneDataType.values()){
+                if(geneDT.name().equals(line))
+                    res = geneDT;
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally{
+            geneDataType = (GeneDataType)res;
+        }
+
+        switch (geneDataType) {
+            case Integer:
+                dataType = Integer.class;
+                break;
+        
+            case Character:
+                dataType = Character.class;
+                break;
+                
+            case Float:
+                dataType = Float.class;
+                break;
+
+            case Boolean:
+                dataType = Boolean.class;
+                break;
+
+            case Double:
+                dataType = Double.class;
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public <T> T getMaxValue(){
+        return (T)maxValue;
+
+    public void test(){
+        Double v = getMaxValue();
+    }
+
+}
+
 enum CrossOverType{
     OnePointCrossOver,
     NPointCrossOver,
@@ -148,4 +211,12 @@ enum MutationType{
     SingleBitInversion,
     BitWisInversion,
     RandomSelection
+}
+
+enum GeneDataType{
+    Integer,
+    Character,
+    Float,
+    Boolean,
+    Double
 }
