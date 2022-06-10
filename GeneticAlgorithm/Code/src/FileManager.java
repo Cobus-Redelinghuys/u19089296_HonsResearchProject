c00365b08d2863da137b22ed80dd4230138a7518
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-06-11 00:52:02 +0200

    JSON library

--- GeneticAlgorithm/Code/src/FileManager.java
+++ GeneticAlgorithm/Code/src/FileManager.java
@@ -1,3 +1,151 @@
+import java.io.FileReader;
+import java.util.Random;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class FileManager {
     
+}
+
+
+class GeneticAlgorithmConfig{
+    public final static int populationSize;
+    public static final int numGenerations;
+    public static final int selectionSize;
+    public static final double reproductionProp;
+    public static final double crossoverProp;
+    public static final double mutationProp;
+    public static final CrossOverType crossOverType;
+    public static final MutationType mutationType;
+    public static final long seed;
+    private static final Random random;
+
+    static{
+        JSONParser jsonParser = new JSONParser();
+        Object obj = null;
+        try{
+            obj = jsonParser.parse(new FileReader("GeneticAlgorithmConfig.json"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = (JSONObject)obj;
+        Object res = null;
+            
+        try{
+            res = jsonObject.get("populationSize");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 200;
+        }finally{
+            populationSize = ((Long)res).intValue();    
+        }
+            
+        try{
+            res = jsonObject.get("numGenerations");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 200;
+        } finally{
+            numGenerations = ((Long)res).intValue();
+        }
+
+        try{
+            res = jsonObject.get("selectionSize");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 200;
+        } finally{
+            selectionSize = ((Long)res).intValue();
+        }
+
+        try{
+            res = jsonObject.get("reproductionProp");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 0.5;
+        } finally{
+            reproductionProp = (double)res;
+        }
+
+        try{
+            res = jsonObject.get("crossoverProp");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 0.5;
+        } finally{
+            crossoverProp = (double)res;
+        }
+
+        try{
+            res = jsonObject.get("mutationProp");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 0.5;
+        } finally{
+            mutationProp = (double)res;
+        }
+            
+        try{
+            String line = (String)jsonObject.get("crossOverType");
+            for(CrossOverType crossOverType: CrossOverType.values()){
+                if(crossOverType.name().equals(line))
+                    res = crossOverType;
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+            res = CrossOverType.OnePointCrossOver;
+        } finally{
+            crossOverType = (CrossOverType)res;
+        }
+
+        try{
+            String line = (String)jsonObject.get("mutationType");
+            for(MutationType mutationType: MutationType.values()){
+                if(mutationType.name().equals(line))
+                    res = mutationType;
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+            res = MutationType.BitWisInversion;
+        } finally{
+            mutationType = (MutationType)res;
+        }
+
+        try{
+            res = jsonObject.get("seed");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 200;
+        } finally{
+            seed = (long)res;
+        }
+        random = new Random(seed);
+    }
+
+    public static int nextInt(int bound){
+        return random.nextInt(bound);
+    }
+
+    public static boolean nextBoolean(){
+        return random.nextBoolean();
+    }
+
+    public static double nextDouble(double bound){
+        return random.nextDouble(bound);
+    }
+}
+
+enum CrossOverType{
+    OnePointCrossOver,
+    NPointCrossOver,
+    SegmentedCrossOver,
+    UniformCrossOver,
+    ShuffleCrossOver
+}
+
+enum MutationType{
+    SingleBitInversion,
+    BitWisInversion,
+    RandomSelection
 }
