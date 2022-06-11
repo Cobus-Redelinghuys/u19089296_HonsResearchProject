39f4e5347a49da3a4e12303275095c08841f6a15
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-06-11 23:18:47 +0200

    Added fix

--- GeneticAlgorithm/Code/src/App.java
+++ GeneticAlgorithm/Code/src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(GeneticAlgorithmConfig.crossoverProp);
         System.out.println(GeneticAlgorithmConfig.crossOverType);
-        System.out.println(ChromosomeConfig.geneConfigs[0].maxValue);
+        System.out.println(ChromosomeConfig.geneConfigs[0].maxValue());
     }
 }
