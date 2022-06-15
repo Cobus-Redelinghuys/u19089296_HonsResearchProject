08aef638ba96191c2c15afb69ccdb2ee6f732d08
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-06-16 01:25:15 +0200

    Added tester class for testing implemented functions

--- GeneticAlgorithm/Code/src/App.java
+++ GeneticAlgorithm/Code/src/App.java
@@ -14,24 +14,6 @@ public class App {
         System.out.println(GeneticAlgorithmConfig.crossoverProp);
         System.out.println(GeneticAlgorithmConfig.crossOverType);
         System.out.println(ChromosomeConfig.geneConfigs[0].maxValue());
-
-        System.out.println(Integer.toBinaryString(-100).length());
-
-        System.out.println(GeneDataType.Integer.convertToBinary((-100)));
-        System.out.println((Integer)GeneDataType.Integer.convertFromBin(GeneDataType.Integer.convertToBinary(-100)));
-
-        System.out.println(GeneDataType.Character.convertToBinary('a'));
-        String bitString = GeneDataType.Character.convertToBinary('a');
-        System.out.println((Integer)GeneDataType.Integer.convertFromBin(bitString));
-        System.out.println((Character)GeneDataType.Character.convertFromBin(bitString));
-
-        System.out.println(GeneDataType.Float.convertToBinary(5.4f));
-        System.out.println((Float)GeneDataType.Float.convertFromBin(GeneDataType.Float.convertToBinary(5.4f)));
-
-        System.out.println(GeneDataType.Boolean.convertToBinary(true));
-        System.out.println((Boolean)GeneDataType.Boolean.convertFromBin(GeneDataType.Boolean.convertToBinary(true)));
-
-        System.out.println(GeneDataType.Double.convertToBinary(5.4));
-        System.out.println((Double)GeneDataType.Double.convertFromBin(GeneDataType.Double.convertToBinary(5.4)));
     }
+
 }
