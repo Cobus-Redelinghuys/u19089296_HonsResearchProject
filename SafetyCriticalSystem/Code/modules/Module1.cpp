f4ab661709e0ee6ee360768912764339980cd22a
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-06-22 01:22:57 +0200

    Implemted the first safety critical module

---
+++ SafetyCriticalSystem/Code/modules/Module1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int readConstant(){
+    fstream newfile;
+    newfile.open("config.txt");
+    int res = 0;
+    if(newfile.is_open()){
+        string tp;
+        getline(newfile, tp);
+        res = stoi(tp);
+        newfile.close();
+    }
+    return res;
+}
+
+class Number{
+    public:
+        virtual void print() = 0;
+};
+
+class GreaterThan: public Number{
+    public:
+        void print(){
+            cout << "GreaterThan" << endl;
+        }
+};
+
+class LessThan: public Number{
+    public:
+        void print(){
+            cout << "LessThan" << endl;
+        } 
+};
+
+int main(int argc, char *argv[]){
+    int n = readConstant();
+    int v = stoi(argv[1]);
+    Number* obj;
+    if(n > v){
+        obj = new GreaterThan();
+    }
+    if(n < v){
+        obj = new LessThan();
+    }
+    obj->print();
+    delete obj;
+    return 0;
+}
