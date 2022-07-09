cf4f25faff4c66b5bea59ad2f537a39e5e6bdf51
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-07-09 19:19:05 +0200

    Implemented LTL component of the fitness class

--- GeneticAlgorithm/Code/src/Fitness.java
+++ GeneticAlgorithm/Code/src/Fitness.java
@@ -9,7 +9,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Fitness {
-    public static float determineFitness(Chromosome input){
+    public static double determineFitness(Chromosome input){
         FileManager.writeChromosomeToFile(input);
         executeSystem();
         ModuleReturns[] output = null;
@@ -24,7 +24,10 @@ public class Fitness {
             System.out.println(moduleReturns);
         }
 
-        return 0;
+        double result = 0;
+        result += LTL(output);
+
+        return result;
     }
 
     private static void executeSystem(){
@@ -74,6 +77,12 @@ public class Fitness {
         }
         return tempArr;
     }
+
+    private static double LTL(ModuleReturns[] output){
+        return FitnessConfig.determineFitness(output);
+    }
+
+
 }
 
 @SuppressWarnings({"rawtypes", "unchecked"})
@@ -95,6 +104,7 @@ class ModuleReturns{
     public final String stdout;
     public final String stderr;
     public final long executionDuration;
+    public final long exitValue;
 
     public ModuleReturns(JSONObject obj) throws MalformedOutput{
         if(!obj.containsKey("stdout") || !obj.containsKey("stderr") || !obj.containsKey("duration"))
@@ -104,6 +114,7 @@ class ModuleReturns{
             stdout = (String)obj.get("stdout");
             stderr = (String)obj.get("stderr");
             executionDuration = (Long)obj.get("duration");
+            exitValue = (Long)obj.get("exitvalue");
         } catch(Exception e){
             e.printStackTrace();
             throw new MalformedOutput();
@@ -122,5 +133,295 @@ class ModuleReturns{
         String res = "";
         res += "stdout: " + stdout + "stderr: " + stderr + "execution duration: " + executionDuration;
         return res;
+    }
+}
+
+class FitnessConfig{
+    public static final FitnessConfigField Safety;
+    public static final FitnessConfigField Livelyness;
+    public static final FitnessConfigField SegFault;
+    public static final FitnessConfigField Exceptions;
+    public static final ExecutionTimeField ExecutionTime;
+    public static final IllegalOutputField IllegalOutput;
+    public static final double LTLWeight;
+    public static final double GWeight;
+    public static final double MWeight;
+
+    static{
+        JSONParser jsonParser = new JSONParser();
+        Object obj = null;
+        try{
+            obj = jsonParser.parse(new FileReader("FitnessConfig.json"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = (JSONObject)obj;
+        Object res = null;
+        try{
+            res = new FitnessConfigField((JSONObject)jsonObject.get("Safety"));
+        } catch (Exception e){
+            e.printStackTrace();
+            res = new FitnessConfigField();
+        } finally{
+            Safety = (FitnessConfigField)res;
+        }
+
+        try{
+            res = new FitnessConfigField((JSONObject)jsonObject.get("Livelyness"));
+        } catch (Exception e){
+            e.printStackTrace();
+            res = new FitnessConfigField();
+        } finally{
+            Livelyness = (FitnessConfigField)res;
+        }
+
+        try{
+            res = new FitnessConfigField((JSONObject)jsonObject.get("SegFault"));
+        } catch (Exception e){
+            e.printStackTrace();
+            res = new FitnessConfigField();
+        } finally{
+            SegFault = (FitnessConfigField)res;
+        }
+
+        try{
+            res = new FitnessConfigField((JSONObject)jsonObject.get("Exceptions"));
+        } catch (Exception e){
+            e.printStackTrace();
+            res = new FitnessConfigField();
+        } finally{
+            Exceptions = (FitnessConfigField)res;
+        }
+
+        try{
+            res = new ExecutionTimeField((JSONObject)jsonObject.get("ExecutionTime"));
+        } catch (Exception e){
+            e.printStackTrace();
+            res = new ExecutionTimeField(); 
+        } finally{
+            ExecutionTime = (ExecutionTimeField)res;
+        }
+
+        try{
+            res = new IllegalOutputField((JSONObject)jsonObject.get("IllegalOutput"));
+        } catch (Exception e){
+            e.printStackTrace();
+            res = new IllegalOutputField();
+        } finally{
+            IllegalOutput = (IllegalOutputField)res;
+        }
+
+        try{
+            res = (Double)jsonObject.get("LTLWeight");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 0.5;
+        } finally{
+            LTLWeight = (Double)res;
+        }
+
+        try{
+            res = (Double)jsonObject.get("MWeight");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 0.5;
+        } finally{
+            MWeight = (Double)res;
+        }
+
+        try{
+            res = (Double)jsonObject.get("GWeight");
+        } catch(Exception e){
+            e.printStackTrace();
+            res = 0.5;
+        } finally{
+            GWeight = (Double)res;
+        }
+    }
+
+    public static double determineFitness(ModuleReturns[] output){
+        double result = 0;
+        result += Safety(output);
+        result += Livelyness(output);
+        result += SegFault(output);
+        result += Exception(output);
+        result += ExecutionTime(output);
+        result += IllegalOutput(output);
+        return FitnessConfig.LTLWeight * result;
+    }
+
+    private static double Safety(ModuleReturns[] output){
+        if(!Safety.enabled){
+            return 0;
+        }
+        double result = 0;
+        for(ModuleReturns moduleReturns: output){
+            if(!(moduleReturns.stderr.isBlank() && moduleReturns.stderr.isEmpty())){
+                result += 1;
+            }
+            if(moduleReturns.stdout.toUpperCase().contains("EXCEPTION")){
+                result += 1;
+            }
+        }
+
+        return Safety.weight * result/(output.length * 2);
+    }
+
+    private static double Livelyness(ModuleReturns[] output){
+        if(!Livelyness.enabled)
+            return 0;
+
+        double result = 0;
+        for(ModuleReturns moduleReturns: output){
+            if(moduleReturns.exitValue != 0){
+                result += 1;
+            }
+        }
+
+        return Livelyness.weight * result / output.length;
+    }
+
+    private static double SegFault(ModuleReturns[] output){
+        if(Safety.enabled)
+            return 0;
+
+        if(!SegFault.enabled)
+            return 0;
+
+        double result = 0;
+        for(ModuleReturns moduleReturns: output){
+            if(moduleReturns.stderr.toLowerCase().contains("segfault") || moduleReturns.stderr.toLowerCase().contains("segmentation fault") || moduleReturns.exitValue == 139){
+                result += 1;
+            }
+        }
+        return SegFault.weight * result / output.length;
+    }
+
+    private static double Exception(ModuleReturns[] output){
+        if(Safety.enabled)
+            return 0;
+
+        if(!SegFault.enabled)
+            return 0;
+
+        double result = 0;
+        for(ModuleReturns moduleReturns: output){
+            if(moduleReturns.stdout.toLowerCase().contains("exception") || moduleReturns.stdout.toLowerCase().contains("exceptions")){
+                result += 1;
+            } else if (moduleReturns.stderr.toLowerCase().contains("exception") || moduleReturns.stderr.toLowerCase().contains("exceptions")){
+                result += 1;
+            }
+        }
+        return Exceptions.weight * result / output.length;
+    }
+
+    private static double ExecutionTime(ModuleReturns[] output){
+        if(!ExecutionTime.enabled)
+            return 0;
+
+        double result = 0;
+        for(ModuleReturns moduleReturns: output){
+            if(moduleReturns.executionDuration > ExecutionTime.maxTime){
+                result += 1;
+            }
+        }
+        return ExecutionTime.weight * result / output.length;
+    }
+
+    private static double IllegalOutput(ModuleReturns[] output){
+        if(!IllegalOutput.enabled)
+            return 0;
+
+        if(IllegalOutput.words.length <=0)
+            return 0;
+
+        double result = 0;
+        for(ModuleReturns moduleReturns: output){
+            for(String word: IllegalOutput.words){
+                if(moduleReturns.stdout.contains(word)){
+                    result += 1;
+                }
+            }
+        }
+        return IllegalOutput.weight * result / (IllegalOutput.words.length * output.length);
+    }
+    
+}
+
+class FitnessConfigField{
+    public final boolean enabled;
+    public final double weight;
+
+    public FitnessConfigField(JSONObject jsonObject) throws MalformedFitnessConfig{
+        try{
+            enabled = (Boolean)jsonObject.get("enabled");
+            weight = ((Long)jsonObject.get("weight")).doubleValue();
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new MalformedFitnessConfig();
+        }
+    }
+
+    public FitnessConfigField(){
+        enabled = false;
+        weight = Double.NaN;
+    }
+}
+
+class IllegalOutputField{
+    public final boolean enabled;
+    public final double weight;
+    public final String[] words;
+
+    public IllegalOutputField(JSONObject jsonObject) throws MalformedFitnessConfig{
+        try{
+            enabled = (Boolean)jsonObject.get("enabled");
+            weight = ((Long)jsonObject.get("weight")).doubleValue();
+            JSONArray arr = (JSONArray)jsonObject.get("words");
+            String[] res = new String[arr.size()];
+            for(int i=0; i < res.length; i++){
+                res[i] = (String)arr.get(i);
+            }
+            words = res;
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new MalformedFitnessConfig();
+        }
+    }
+
+    public IllegalOutputField(){
+        enabled = false;
+        weight = Double.NaN;
+        words = new String[0];
+    }
+}
+
+class ExecutionTimeField{
+    public final boolean enabled;
+    public final double weight;
+    public final long maxTime;
+
+    public ExecutionTimeField(JSONObject jsonObject) throws MalformedFitnessConfig{
+        try{
+            enabled = (Boolean)jsonObject.get("enabled");
+            weight = ((Long)jsonObject.get("weight")).doubleValue();
+            maxTime = (Long)jsonObject.get("maxTime");
+        } catch(Exception e){
+            e.printStackTrace();
+            throw new MalformedFitnessConfig();
+        }
+    }
+
+    public ExecutionTimeField(){
+        enabled = false;
+        weight = Double.NaN;
+        maxTime = 0;
+    }
+
+}
+
+class MalformedFitnessConfig extends Exception{
+    MalformedFitnessConfig(){
+        super("Malformed fitnress config file");
     }
 }
