8ad05aa8e4d68fae02573a075a27036c08ba26aa
Cobus Redelinghuys <72762130+DarkMerlin1@users.noreply.github.com>
2022-07-12 13:02:15 +0200

    Implemented the 4th module

---
+++ SafetyCriticalSystem/Code/modules/Module4/Main.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main{
+    public static int readFile(){
+        int returnVal = 0;
+        try {
+            File myObj = new File("config.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                returnVal = Integer.parseInt(data);
+                break;
+            }
+            myReader.close();
+        } catch (Exception e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return returnVal;
+    }
+    
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[readFile()];
+        for(int i=0; i < arr.length; i++){
+            arr[i] = i;
+        }
+        int index = Integer.parseInt(args[0]);
+        System.out.println(arr[index]);
+
+    }
+}
